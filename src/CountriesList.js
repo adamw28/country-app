7e104289c04b55751dbec7849fa6a9d38877fab7
Adam Więcek <adamw28@gmail.com>
2018-03-05 21:16:41 +0100

    Merge branches

--- src/CountriesList.js
+++ src/CountriesList.js
@@ -2,6 +2,6 @@ import React from 'react';
 import Country from './CountryContainer';
 
 const CountriesList = ({countries}) => {
-	return <ul className={style.countries-list} >{countries.map(country => <Country key={country.id} {...country}/>)}</ul>;}
+	return <ul className="countries-list" >{countries.map(country => <Country key={country.id} {...country}/>)}</ul>;}
 
 export default CountriesList;
--- src/presentational/country-details.component.js
+++ src/presentational/country-details.component.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import style from '../country.css';
 
 const CountryDetails = (props) => (
     <div className="country-wrapper">
